5e5bab063fb4b2a9232e1cf5905e56fb6f669a56
strawgate <williamseaston@gmail.com>
2026-05-01 14:54:45 -0500

    fix: remove read_replication setting that may be causing D1 API errors

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -149,10 +149,6 @@ resource "cloudflare_d1_database" "fleet" {
   account_id = var.cloudflare_account_id
   name       = local.d1_database_name
 
-  read_replication = {
-    mode = "disabled"
-  }
-
   lifecycle {
     prevent_destroy = true
   }
